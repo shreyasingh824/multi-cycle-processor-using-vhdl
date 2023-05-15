41af2a5562668b6423ac64a1a8652c08888d48d8
shreya singh <112194327+shreyasingh824@users.noreply.github.com>
2023-05-16 01:19:49 +0530

    Create regA.vhd

---
+++ regA.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity A is
+Port (
+           clock     : in std_logic;
+           DataIn  : in STD_LOGIC_VECTOR (31 downto 0);
+           DataOut : out STD_LOGIC_VECTOR (31 downto 0)
+
+
+ );
+end A;
+
+architecture Behavioral of A is
+
+begin
+    process(DataIn, clock)
+    begin
+        if(rising_edge(clock)) then
+             DataOut <= DataIn;
+             
+        end if;
+    end process;
+
+
+end Behavioral;
